36bb7c85d163b85230f649b9a8c9057ad5465ee8
cl27chan <127961756+cl27chan@users.noreply.github.com>
2025-09-24 15:16:50 +0800

    Add files via upload

---
+++ Customer/FormJob/LotDetailDisplay.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Customer
+{
+    partial class LotDetailDisplay
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelOverallTotalOutput = new System.Windows.Forms.Label();
+            this.labelOverallTotalDefect = new System.Windows.Forms.Label();
+            this.labelOverallTotalYield = new System.Windows.Forms.Label();
+            this.labelOverallTotalQuantity = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.labelShiftBTotalDefect = new System.Windows.Forms.Label();
+            this.labelShiftBTotalYield = new System.Windows.Forms.Label();
+            this.labelShiftBTotalQuantity = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelShiftATotalDefect = new System.Windows.Forms.Label();
+            this.labelShiftATotalYield = new System.Windows.Forms.Label();
+            this.labelShiftATotalQuantity = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labelOverallTotalOutput);
+            this.panel1.Controls.Add(this.labelOverallTotalDefect);
+            this.panel1.Controls.Add(this.labelOverallTotalYield);
+            this.panel1.Controls.Add(this.labelOverallTotalQuantity);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.labelShiftBTotalDefect);
+            this.panel1.Controls.Add(this.labelShiftBTotalYield);
+            this.panel1.Controls.Add(this.labelShiftBTotalQuantity);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.labelShiftATotalDefect);
+            this.panel1.Controls.Add(this.labelShiftATotalYield);
+            this.panel1.Controls.Add(this.labelShiftATotalQuantity);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(5, 5);
+            this.panel1.Margin = new System.Windows.Forms.Padding(5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(263, 651);
+            this.panel1.TabIndex = 0;
+            // 
+            // labelOverallTotalOutput
+            // 
+            this.labelOverallTotalOutput.AutoSize = true;
+            this.labelOverallTotalOutput.Location = new System.Drawing.Point(5, 495);
+            this.labelOverallTotalOutput.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.labelOverallTotalOutput.Name = "labelOverallTotalOutput";
+            this.labelOverallTotalOutput.Size = new System.Drawing.Size(117, 24);
+            this.labelOverallTotalOutput.TabIndex = 12;
+            this.labelOverallTotalOutput.Text = "Total Output:";
+            // 
+            // labelOverallTotalDefect
+            // 
+            this.labelOverallTotalDefect.AutoSize = true;
+            this.labelOverallTotalDefect.Location = new System.Drawing.Point(5, 466);
+            this.labelOverallTotalDefect.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.labelOverallTotalDefect.Name = "labelOverallTotalDefect";
+            this.labelOverallTotalDefect.Size = new System.Drawing.Size(114, 24);
+            this.labelOverallTotalDefect.TabIndex = 11;
+            this.labelOverallTotalDefect.Text = "Total Defect:";
+            // 
+            // labelOverallTotalYield
+            // 
+            this.labelOverallTotalYield.AutoSize = true;
+            this.labelOverallTotalYield.Location = new System.Drawing.Point(5, 437);
+            this.labelOverallTotalYield.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.labelOverallTotalYield.Name = "labelOverallTotalYield";
+            this.labelOverallTotalYield.Size = new System.Drawing.Size(108, 24);
+            this.labelOverallTotalYield.TabIndex = 10;
+            this.labelOverallTotalYield.Text = "Total Yield :";
+            // 
+            // labelOverallTotalQuantity
+            // 
+            this.labelOverallTotalQuantity.AutoSize = true;
+            this.labelOverallTotalQuantity.Location = new System.Drawing.Point(5, 408);
+            this.labelOverallTotalQuantity.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.labelOverallTotalQuantity.Name = "labelOverallTotalQuantity";
+            this.labelOverallTotalQuantity.Size = new System.Drawing.Size(89, 24);
+            this.labelOverallTotalQuantity.TabIndex = 9;
+            this.labelOverallTotalQuantity.Text = "Total Qty:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(5, 379);
+            this.label6.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 24);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Overall";
+            // 
+            // labelShiftBTotalDefect
+            // 
+            this.labelShiftBTotalDefect.AutoSize = true;
+            this.labelShiftBTotalDefect.Location = new System.Drawing.Point(5, 279);
+            this.labelShiftBTotalDefect.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.labelShiftBTotalDefect.Name = "labelShiftBTotalDefect";
+            this.labelShiftBTotalDefect.Size = new System.Drawing.Size(114, 24);
+            this.labelShiftBTotalDefect.TabIndex = 7;
+            this.labelShiftBTotalDefect.Text = "Total Defect:";
+            // 
+            // labelShiftBTotalYield
+            // 
+            this.labelShiftBTotalYield.AutoSize = true;
+            this.labelShiftBTotalYield.Location = new System.Drawing.Point(5, 250);
+            this.labelShiftBTotalYield.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.labelShiftBTotalYield.Name = "labelShiftBTotalYield";
+            this.labelShiftBTotalYield.Size = new System.Drawing.Size(108, 24);
+            this.labelShiftBTotalYield.TabIndex = 6;
+            this.labelShiftBTotalYield.Text = "Total Yield :";
+            // 
+            // labelShiftBTotalQuantity
+            // 
+            this.labelShiftBTotalQuantity.AutoSize = true;
+            this.labelShiftBTotalQuantity.Location = new System.Drawing.Point(5, 221);
+            this.labelShiftBTotalQuantity.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.labelShiftBTotalQuantity.Name = "labelShiftBTotalQuantity";
+            this.labelShiftBTotalQuantity.Size = new System.Drawing.Size(89, 24);
+            this.labelShiftBTotalQuantity.TabIndex = 5;
+            this.labelShiftBTotalQuantity.Text = "Total Qty:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(5, 192);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Shift B";
+            // 
+            // labelShiftATotalDefect
+            // 
+            this.labelShiftATotalDefect.AutoSize = true;
+            this.labelShiftATotalDefect.Location = new System.Drawing.Point(5, 96);
+            this.labelShiftATotalDefect.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.labelShiftATotalDefect.Name = "labelShiftATotalDefect";
+            this.labelShiftATotalDefect.Size = new System.Drawing.Size(114, 24);
+            this.labelShiftATotalDefect.TabIndex = 3;
+            this.labelShiftATotalDefect.Text = "Total Defect:";
+            // 
+            // labelShiftATotalYield
+            // 
+            this.labelShiftATotalYield.AutoSize = true;
+            this.labelShiftATotalYield.Location = new System.Drawing.Point(5, 67);
+            this.labelShiftATotalYield.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.labelShiftATotalYield.Name = "labelShiftATotalYield";
+            this.labelShiftATotalYield.Size = new System.Drawing.Size(108, 24);
+            this.labelShiftATotalYield.TabIndex = 2;
+            this.labelShiftATotalYield.Text = "Total Yield: ";
+            // 
+            // labelShiftATotalQuantity
+            // 
+            this.labelShiftATotalQuantity.AutoSize = true;
+            this.labelShiftATotalQuantity.Location = new System.Drawing.Point(5, 38);
+            this.labelShiftATotalQuantity.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.labelShiftATotalQuantity.Name = "labelShiftATotalQuantity";
+            this.labelShiftATotalQuantity.Size = new System.Drawing.Size(89, 24);
+            this.labelShiftATotalQuantity.TabIndex = 1;
+            this.labelShiftATotalQuantity.Text = "Total Qty:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Shift A";
+            // 
+            // LotDetailDisplay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCyan;
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "LotDetailDisplay";
+            this.Size = new System.Drawing.Size(285, 661);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Label labelOverallTotalOutput;
+        public System.Windows.Forms.Label labelOverallTotalDefect;
+        public System.Windows.Forms.Label labelOverallTotalYield;
+        public System.Windows.Forms.Label labelOverallTotalQuantity;
+        private System.Windows.Forms.Label label6;
+        public System.Windows.Forms.Label labelShiftBTotalDefect;
+        public System.Windows.Forms.Label labelShiftBTotalYield;
+        public System.Windows.Forms.Label labelShiftBTotalQuantity;
+        private System.Windows.Forms.Label label5;
+        public System.Windows.Forms.Label labelShiftATotalDefect;
+        public System.Windows.Forms.Label labelShiftATotalYield;
+        public System.Windows.Forms.Label labelShiftATotalQuantity;
+    }
+}
