12af0784f73ccd65d1789511051debba851ebda9
cl27chan <127961756+cl27chan@users.noreply.github.com>
2025-09-24 18:01:51 +0800

    Add files via upload

---
+++ Product/FormSetup/tabPageSetup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Product.FormSetup
+{
+    public partial class tabPageSetup : UserControl
+    {
+        public tabPageSetup()
+        {
+            InitializeComponent();
+        }
+    }
+}
